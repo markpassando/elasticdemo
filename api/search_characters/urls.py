07261aaa0d940e7f6726ddb036469d3fff76a109
MarkPassando <markpassando@gmail.com>
2018-11-03 15:41:50 -0400

    Add GET /users/<user> route

--- api/search_characters/urls.py
+++ api/search_characters/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('getUsers/', views.GetUsers.as_view()),
+  path('users/<user>', views.UserSearch.as_view()),
 ]
--- api/search_characters/views.py
+++ api/search_characters/views.py
@@ -15,3 +15,15 @@ class GetUsers(APIView):
         except Exception as exception:
             print(exception)
             return Response({'error': str(exception)})
+
+class UserSearch(APIView):
+
+  def get(self, request, **kwargs):
+        """Search for a User"""
+        try:
+            users = models.User.objects.all().values()
+
+            return Response({'data': list(users)})
+        except Exception as exception:
+            print(exception)
+            return Response({'error': str(exception)})
